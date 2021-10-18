d6bd3cd03e3c80d965b336857b82e1b712b1e60c
Rakesh Roy <66549807+yrs-rosh@users.noreply.github.com>
2021-10-18 21:22:57 +0530

    added nameshortner.js file

---
+++ nameshortner.js
@@ -0,0 +1,13 @@
+!// AUTHOR: Rakesh Kumar
+// JavaScript Concept: short your name
+// GITHUB: https://github.com/Reayz
+
+// shorten your name in javascript
+
+var name = "Rakesh"
+var sname = name.splice(0,4);
+console.log(sname);
+
+
+// output
+// Rake
